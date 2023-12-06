9b87009469b8da84b8bb976213d4f29bc41c5173
hhg <huhaigang@uan.cn>
2023-12-06 15:37:09 +0800

    整合项目

---
+++ kafkaTemplete/src/main/java/com/kafkatemplete/KafkaTempleteApplication.java
@@ -0,0 +1,13 @@
+package com.kafkatemplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTempleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTempleteApplication.class, args);
+    }
+
+}
